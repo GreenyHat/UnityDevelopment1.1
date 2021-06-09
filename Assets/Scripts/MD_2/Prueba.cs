0ddaa48e4d9ccf3cfe138b3133e2b64b90567a52
GreenyHat <aramirezgarcia95@gmail.com>
2021-06-09 09:11:39 +0200

    update

---
+++ Assets/Scripts/MD_2/Prueba.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        print("Holaaaa");
+        print("Holaaaa");
+        print("Holaaaa por tercera vez.");
+        print("Esto es para ver ssi me acuerdo");
+    }
+}
