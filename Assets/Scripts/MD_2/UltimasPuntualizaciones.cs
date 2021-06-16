e8e7c22c120972be1508ae66e7afe9faff5ff8db
GreenyHat <aramirezgarcia95@gmail.com>
2021-06-16 11:19:10 +0200

    addTestCoroutine

--- Assets/Scripts/MD_2/UltimasPuntualizaciones.cs
+++ Assets/Scripts/MD_2/UltimasPuntualizaciones.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class UltimasPuntualizaciones : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    Rigidbody rB;
+
     void Start()
     {
-        
+        Invoke("BeginRun", 0.3f);//Esta forma de invocar a la funcion es menos optima
+        //debido a que la forma de leer las cadenas de caracteres es mas lenta. Mejor utilizar una corrutina
+        InvokeRepeating("JumpLoop", 0.4f, 2f);//Este es mas interesante que el invoke el tercer parametro
+        //Es para establecer las veces que se repetira
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    void BeginRun()
+    {
+        rB.velocity = new Vector3(5, 0, 0);
+    }
+
+    void JumpLoop()
+    {
+        rB.AddForce(new Vector3(0, 170, 0));
+    }
+
+    //Repaso de las corrutinas--> TestCoroutine.cs
 }
